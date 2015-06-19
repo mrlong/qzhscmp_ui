2a150c1ce7071dffa090ba9a9d2659554618e4b6
mrlong <mrlong.com@gmail.com>
2015-06-19 16:32:41 +0800

    sss

--- router-ent.js
+++ router-ent.js
@@ -8,6 +8,30 @@ router.use(function(req,res,next){
   next(); 
 });
 
+//工程管理
+router.use('/project/detail/:guid',function(req,res,next){
+  res.loadview('project/detail.html');
+});
+
+router.use('/project',function(req,res,next){
+  res.loadview('project/project.html');
+});
+
+//企业成员
+router.use('/member/add',function(req,res,next){
+  res.loadview('member/add.html');
+});
+
+router.use('/member/edit',function(req,res,next){
+  res.loadview('member/edit.html');
+});
+
+
+router.use('/member',function(req,res,next){
+  res.loadview('member/index.html');
+});
+
+
 //  /ent/
 router.use('/',function(req,res,next){
   res.loadview('index.html');
